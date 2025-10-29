0fa32a3a5168b09869cd3826dcea1c72ce0c257f
PhelanUX <s4kyo4s@gmail.com>
2025-10-29 17:00:34 +0700

    1:03:40 blockchain

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  mode: "jit",
+  darkMode: false,
   theme: {
-    extend: {},
+    fontFamily: {
+      display: ["Open Sans", "sans-serif"],
+      body: ["Open Sans", "sans-serif"],
+    },
+    extend: {
+      screens: {
+        mf: "990px",
+      },
+      keyframes: {
+        "slide-in": {
+          "0%": {
+            transform: "translateX(120%)",
+          },
+          "100%": {
+            transform: "translateX(0%)",
+          },
+        },
+      },
+      animation: {
+        "slide-in": "slide-in 0.5s ease-out",
+      },
+    },
   },
-  plugins: [],
-}
-
+  plugins: [require("@tailwindcss/forms")],
+};
