45c1336524946c0fda5a398c4eea6a99450c9e59
PhelanUX <s4kyo4s@gmail.com>
2025-11-05 16:01:19 +0700

    2:22:00

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -6,7 +6,7 @@ module.exports = {
   networks: {
     coston: {
       url: "https://coston-api.flare.network/ext/C/rpc",
-      accounts: [process.env.PRIVATE_KEY],
+      //accounts: [process.env.PRIVATE_KEY],
       chainId: 16
     }
   }
