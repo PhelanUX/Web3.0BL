5acb108242546b094fb58a09af87cc554909430f
PhelanUX <s4kyo4s@gmail.com>
2025-11-13 10:15:22 +0700

    add slogan

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,12 +3,15 @@ import logo from '../../images/logo.png';
 const Footer = () => {
     return(
         <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer">
-            <div className="w-full flex sm:flex-row flex-col justify-center items-center my-4">
+            <div className="w-full flex sm:flex-row flex-col justify-between items-center my-4">
                 <div className="flex flex-[0.5] justify-center items-center">
                     <img src={logo} alt='logo' className='w-32'/>
                 </div>
-                 <div className='flex justify-center items-center flex-col mt-5'>
-                <p className='text-white text-sm text-center'>Come join us</p>
+                 <div className='flex flex-1 justify-evenly items-center flex-wrap sm:mt-0 mt-5 w-full'>
+                <p className='flex-[0.6] text-white text-sm text-center'>This is a web create for transfer the testnet coin, we are developing it to 
+                suitable for other coin as much as we can. Soon, you can transfer other coins. We dont take any fee for any transaction, it is 
+                free for everyone.
+                </p>
             </div>
                 {/* <div className='flex flex-1 justify-evenly items-center flex-wrap sm:mt-0 mt-5 w-full'>
                     <p className='text-white text-base text-center mx-2 cursor-pointer'>Market</p>
@@ -17,13 +20,13 @@ const Footer = () => {
                     <p className='text-white text-base text-center mx-2 cursor-pointer'>Wallet</p>
                 </div> */}
             </div>
-            <div className='flex justify-center items-center flex-col mt-5'>
+            {/* <div className='flex justify-center items-center flex-col mt-5'>
                 <p className='text-white text-sm text-center'>Come join us</p>
                 <p className='text-white text-sm text-center'>ltn@gmail.com</p>
-            </div>
+            </div> */}
             <div className='sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5'/>
             <div className='sm:w-[90%] w-full flex justify-between items-center mt-3'>
-                <p className='text-white text-sm text-center'></p>
+                <p className='text-white text-sm text-center'>Come join us <br/> ltn@gmail.com</p>
                 <p className='text-white text-sm text-center'>Alright reserved</p>
             </div>
         </div>
