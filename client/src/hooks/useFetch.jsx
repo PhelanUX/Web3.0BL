734f8e45f9349d6cecbe5c8cc4148052617ec561
PhelanUX <s4kyo4s@gmail.com>
2025-11-17 11:23:22 +0700

    add comment in useFetch

--- client/src/hooks/useFetch.jsx
+++ client/src/hooks/useFetch.jsx
@@ -6,12 +6,14 @@ const useFetch = ({keyword}) =>{
     const [prevKeyword, setPrevKeyword] = useState("");
     const fetchGifs = async () =>{
         try {
+            // Sử dụng từ khóa để tìm GIF liên quan
             const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${keyword.split("").join("")}&limit=1`)
 
             const { data } = await response.json();
 
             setGifUrl(data[0]?.images?.downsized_medium?.url)
         } catch (error) {
+            // su dung gif mac dinh khi 429
             setGifUrl("https://i.pinimg.com/originals/73/d3/a1/73d3a14d212314ab1f7268b71d639c15.gif")
             if (error.message.includes("429")) {
                 console.warn("Rate limit exceeded — switching to fallback GIF");
@@ -22,6 +24,7 @@ const useFetch = ({keyword}) =>{
 
     useEffect(() =>{
         // if(keyword) fetchGifs();
+        // Chỉ fetch khi từ khóa thay đổi
           if (keyword && keyword !== prevKeyword) {
             fetchGifs();
             setPrevKeyword(keyword);
