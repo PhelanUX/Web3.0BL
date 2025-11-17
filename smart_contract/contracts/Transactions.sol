55f5c4d4d6024053275ec4f711bdbcfb0153b4b1
PhelanUX <s4kyo4s@gmail.com>
2025-11-17 11:27:19 +0700

    add comment in Transactions.sol

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.0;
 
 contract Transactions {
     uint256 transactionCount;
-
+    // SỰ KIỆN GIAO DỊCH
     event Transfer (address from, address receiver, uint amount, string message, uint256 timestamp, string keyword);
-
+    // CẤU TRÚC GIAO DỊCH
     struct TransferStruct {
         address sender;
         address receiver;
@@ -17,7 +17,7 @@ contract Transactions {
     }
 
     TransferStruct[] transactions;
-
+    // LƯU GIAO DỊCH VÀO BLOCKCHAIN
     function addToBlockchain(address payable receiver, uint amount, string memory message, string memory keyword) public {
         transactionCount +=1;
 
@@ -25,11 +25,11 @@ contract Transactions {
 
         emit Transfer(msg.sender, receiver, amount, message, block.timestamp, keyword);
     }
-
+    // LẤY TẤT CẢ GIAO DỊCH
     function getAllTransactions() public view returns (TransferStruct[] memory) {
         return transactions;
     }
-
+    // LẤY SỐ LƯỢNG GIAO DỊCH
     function getTransactionCount() public view returns (uint256) {
         return transactionCount;
     }
