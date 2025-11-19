c84ce9dece6c7ca9c8968021d11ae128c78e5e8f
PhelanUX <s4kyo4s@gmail.com>
2025-11-19 10:38:36 +0700

    add comment to useFetch.jsx

--- client/src/hooks/useFetch.jsx
+++ client/src/hooks/useFetch.jsx
@@ -8,23 +8,23 @@ const useFetch = ({keyword}) =>{
         try {
             // Sử dụng từ khóa để tìm GIF liên quan
             const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${keyword.split("").join("")}&limit=1`)
-
+            
             const { data } = await response.json();
-
+            // Lấy URL của GIF đầu tiên từ kết quả
             setGifUrl(data[0]?.images?.downsized_medium?.url)
         } catch (error) {
             // su dung gif mac dinh khi 429
             setGifUrl("https://i.pinimg.com/originals/73/d3/a1/73d3a14d212314ab1f7268b71d639c15.gif")
+            // neu bi loi 429, chuyen sang xai gif mac dinh
             if (error.message.includes("429")) {
                 console.warn("Rate limit exceeded — switching to fallback GIF");
             }
                 setGifUrl("https://i.pinimg.com/originals/73/d3/a1/73d3a14d212314ab1f7268b71d639c15.gif");
         }
     }
-    //
+    // Chỉ fetch khi từ khóa thay đổi
     useEffect(() =>{
         // if(keyword) fetchGifs();
-        // Chỉ fetch khi từ khóa thay đổi
           if (keyword && keyword !== prevKeyword) {
             fetchGifs();
             setPrevKeyword(keyword);
