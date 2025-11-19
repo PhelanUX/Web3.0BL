fe7362b49a6efa68db24238892da32862f567068
PhelanUX <s4kyo4s@gmail.com>
2025-11-19 10:41:02 +0700

    add comment to hardhat.config.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -2,11 +2,15 @@ require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
 
 module.exports = {
+  // Phien ban Solidity
   solidity: "0.8.20",
   networks: {
     coston: {
+      // Coston testnet RPC URL
       url: "https://coston-api.flare.network/ext/C/rpc",
+      // Tai khoan tu bien moi truong (mac dinh neu trinh duyet khong co tai khoan nay thi se tu lien ket den tai khoan co trong trinh duyet do)
       accounts: [process.env.PRIVATE_KEY],
+      // Chain ID cua Coston testnet
       chainId: 16
     }
   }
